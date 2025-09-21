68e729ce398bfa94fbab8e75b6f55f5cff6c121c
lucassantos31 <103789122+lucassantos31@users.noreply.github.com>
2025-09-21 07:10:27 -0300

    Use total energies for dynamic terms and add gravity test

--- calcularDinamica.m
+++ calcularDinamica.m
@@ -2,7 +2,7 @@ function Dinamica = calcularDinamica(Pcm)
     syms g
 
     
-    n = Pcm{6}.n;
+    n = Pcm{end}.n;
     syms Massa [1,n]
     syms Ixx   [n,1]
     syms Ixy   [n,1]
@@ -22,7 +22,6 @@ function Dinamica = calcularDinamica(Pcm)
     end
     
 
-    H1_aux             = sym(zeros(n,1));    
     Dinamica           = struct();
     Dinamica.G         = sym(zeros(n,1));
     Dinamica.M         = sym(zeros(n,n));
@@ -34,26 +33,33 @@ function Dinamica = calcularDinamica(Pcm)
     Dinamica.EpT       = 0;
     Dinamica.gravidade = [0 0 g];
 
-    
+    q  = Pcm{end}.juntas(1,:);
+    dq = Pcm{end}.juntas(2,:);
+
     for i = 1:n
-        q              = Pcm{i}.juntas(1,:);
-        dq             = Pcm{i}.juntas(2,:);
         Iaux           = Pcm{i}.T(1:3,1:3) * I(i) * Pcm{i}.T(1:3,1:3);
         Dinamica.Ep(i) = (Massa(i)) * Dinamica.gravidade * (Pcm{i}.P);
         Dinamica.Ec(i) = (Massa(i)/2) * transpose(Pcm{i}.V) * Pcm{i}.V + (Massa(i)/2) * transpose(Pcm{i}.W) * Iaux * (Pcm{i}.W);
+        Dinamica.EpT   = Dinamica.EpT + Dinamica.Ep(i);
+        Dinamica.EcT   = Dinamica.EcT + Dinamica.Ec(i);
     end
 
     Dinamica.Lagrangeano = -Dinamica.EcT + Dinamica.EpT;
 
     for i = 1:n
-        Dinamica.G(i,1)  =  diff(Dinamica.Ep(i),q(i));
-        Dinamica.H2(i,1) = -diff(Dinamica.Ec(i),q(i));
-        for j = 1:n
-            Dinamica.M(i,j) = diff(diff(Dinamica.Ec(i),dq(i)),dq(j));
-            H1_aux(j,1) = diff(diff(Dinamica.Ec(i),dq(i)),q(j))*dq(j);
+        Dinamica.G(i,1) = 0;
+        for k = 1:n
+            Dinamica.G(i,1) = Dinamica.G(i,1) + diff(Dinamica.Ep(k), q(i));
         end
 
-        Dinamica.H1(i,1) = sum(H1_aux);
+        Dinamica.H2(i,1) = -diff(Dinamica.EcT, q(i));
+        H1_sum = sym(0);
+        for j = 1:n
+            Dinamica.M(i,j) = diff(diff(Dinamica.Ec(i),dq(i)),dq(j));
+            H1_sum = H1_sum + diff(diff(Dinamica.EcT, dq(i)), q(j)) * dq(j);
+        end
+
+        Dinamica.H1(i,1) = H1_sum;
     end
 
     Dinamica.H = Dinamica.H1 + Dinamica.H2;
